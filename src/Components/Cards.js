9187d70ed4b8f4b364ea881e5d5ec80bf8b1eba8
Franciscofederice <franciscofederice1995@gmail.com>
2021-08-24 15:38:05 -0300

    cards modificadas
    modificacion en las cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -8,21 +8,21 @@ import image3 from '../assets/image3.jpg'
 const cards = [
   {
     id: 1,
-    title: "Fazt Web",
+    title: "Bicicleta 1",
     image: image1,
-    url: "https://faztweb.com",
+    url: "https://coderHouse.com",
   },
   {
     id: 2,
-    title: "Fazt Blog",
+    title: "Bicicleta 2",
     image: image2,
-    url: "https://blog.faztweb.com",
+    url: "https://coderHouse.com",
   },
   {
     id: 3,
-    title: "Fazt Youtube",
+    title: "Bicicleta 3",
     image: image3,
-    url: "https://youtube.com/fazttech",
+    url: "https://https://coderHouse.com",
   },
 ];
 
