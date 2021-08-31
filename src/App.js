f55e2a57a72615e4b047323607a9fe0bb82038e9
Franciscofederice <franciscofederice1995@gmail.com>
2021-08-30 22:46:47 -0300

    cambios de contador

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './Components/NavbarComp';
 import Cards from './Components/Cards';
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
     <div className="App">
+     <ItemCount/>
       <NavbarComp/>
-      <Cards />
+      <Cards/>
+ 
     </div>
   );
 }
