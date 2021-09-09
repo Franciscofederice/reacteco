a4e831583e422df32331b3880d3aee2d433f0698
Franciscofederice <franciscofederice1995@gmail.com>
2021-09-08 21:07:04 -0300

    carrito y mejoras

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComp from './Components/NavbarComp';
-import Cards from './Components/Cards';
-import ItemCount from './Components/ItemCount/ItemCount';
-import ItemDetailContainer from './Components/ItemDeailContainer/ItemDetailContainer';
+import React, { Fragment, useState } from "react";
+import Burger from "./components/Burger";
+import Cart from "./components/Cart";
+import "./styles/styles.css";
+
+// IMPORTO LAS IMAGENES
+import Salteña from "./images/Salteña.png";
+import Porteña from "./images/Porteña.png";
+import Chicken from "./images/Chicken.png";
+import Criolla from "./images/Criolla.png";
+import Patagonia from "./images/patagonia.png";
+
 function App() {
-  const onAdd =(count)=>{
-    console.log(`la cantidad es : ${count}`)
-    alert(`la cantidad es : ${count}`)    
-        
-}
+  const [burgers, setBurgers] = useState([
+    { id: 5, image: Salteña, nombre: "SALTEÑA", precio: 250 },
+    { id: 2, image: Porteña, nombre: "PORTEÑA", precio: 180 },
+    { id: 3, image: Chicken, nombre: "CHICKEN", precio: 150 },
+    { id: 4, image: Criolla, nombre: "CRIOLLA", precio: 200 },
+    { id: 1, image: Patagonia, nombre: "PATAGONIA", precio: 240 },
+  ]);
+
+  const [cart, setCart] = useState([]);
+
   return (
-    <div className="App">
-   
-      <NavbarComp/>
-      <Cards/>
-     <ItemDetailContainer/>
-      <ItemCount initial={1} stock={25} onAdd={onAdd} />
-    
-    </div>
+    <Fragment>
+      {/* SECTION CONTENEDOR GLOBAL */}
+      <section className="container">
+        {/* SECTION DE LA CABECERA */}
+        <section className="container__header">
+          {/* BOTON DE LA CABECERA */}
+          <div className="container__header__menu">
+            <button className="container__header__menu--button"></button>
+          </div>
+
+          {/* TITULO DE LA CABECERA */}
+          <div className="container__header__title">
+            <h3 className="container__header__title--text">HAMBURGER</h3>
+          </div>
+
+          {/* CART DE LA CABECERA */}
+          <div className="container__header__cartDiv">
+            <Cart cart={cart} setCart={setCart} />
+          </div>
+        </section>
+
+        {/* SECTION CONTENEDOR PRINCIPAL */}
+        <section className="container__content">
+
+          <div className="container__content__title">
+            <h1>Aprovecha solo por tiempo limitado combos 50% off!</h1>
+          </div>
+
+          <div className="container__content__listBurgers">
+            {burgers.map((burger) => (
+              <Burger
+                key={burger.id}
+                burger={burger}
+                cart={cart}
+                setCart={setCart}
+                burgers={burgers}
+              />
+            ))}
+          </div>
+
+        </section>
+
+        {/* SECTION FOOTER */}
+        <section className="container__footer">
+          <p>HAMBURGER 2020 - Todos los derechos reservados</p>
+        </section>
+        
+      </section>
+    </Fragment>
   );
 }
 
--- src/utlis/promesas.js
+++
@@ -1,8 +0,0 @@
-
-const prod = {id: 'asdfsadf', url:'https://labicikleta.com/wp-content/uploads/2016/07/FeatureBiciMontana-770x513.jpg' , descripcion: 'bicicleta', price: 123, name: 'producto', stock: 23}
-
-export const getProduc = new Promise((resuelto)=>{
-    setTimeout(() => {
-        resuelto(prod)        
-    }, 5000);
-})
